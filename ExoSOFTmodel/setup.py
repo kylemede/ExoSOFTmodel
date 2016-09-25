cb2860dea5bc04f8a57216f2e4adf9daf2c25c95
kylemede <kylemede@gmail.com>
2016-09-25 19:04:44 +0900

    removed cython from requirements and moved text into setup instead of having a readme for pypi

--- ExoSOFTmodel/setup.py
+++ ExoSOFTmodel/setup.py
@@ -21,14 +21,6 @@ from Cython.Distutils import build_ext
 ext_modules = [
                Extension("cytools",["cytools.pyx"],include_dirs=['.'])               
                ]
-
-## Modifications to get cython code to be documented by Sphinx
-## mods taken from: https://github.com/abingham/cython-sphinx-example
-# By setting this compiler directive, cython will
-# embed signature information in docstrings. Sphinx then knows how to extract
-# and use those signatures.
-for e in ext_modules:
-    e.cython_directives = {"embedsignature": True}
     
 setup(
       name = 'ExoSOFTmodel',
--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@
 from __future__ import absolute_import
 import os
 import sys
-import re
+#import re
+
 
 try:
     from setuptools import setup
@@ -10,37 +11,25 @@ try:
 except ImportError:
     from distutils.core import setup
     setup
+    
+from distutils.extension import Extension
 
 if sys.argv[-1] == "publish":
     os.system("python setup.py sdist upload")
     sys.exit()
 
-# Handle encoding
-major, minor1, minor2, release, serial = sys.version_info
-if major >= 3:
-    def rd(filename):
-        f = open(filename, encoding="utf-8")
-        r = f.read()
-        f.close()
-        return r
-else:
-    def rd(filename):
-        f = open(filename)
-        r = f.read()
-        f.close()
-        return r
-
 setup(    
     name='ExoSOFTmodel', 
     packages =['ExoSOFTmodel'],
-    version="1.0.8", 
+    version="1.0.22", 
     author='Kyle Mede',
     author_email = 'kylemede@gmail.com',
     url = 'https://github.com/kylemede/ExoSOFTmodel',
     license = ['GNU GPLv3'],
     description ='An astronomical model for calculating the predicted astrometry and radial velocity due to a companion.',
-    long_description = rd("README-pypi.rst"),
-    package_data={"": ["LICENSE", "AUTHORS.rst"]},
+    long_description = "For examples of how to use ExoSOFTmodel or"+
+    " read its documentation, please visit:\nhttps://github.com/kylemede/ExoSOFTmodel",
+    package_data={"": ["LICENSE", "AUTHORS.rst","ExoSOFTmodel/cytools.pyx"]},
     include_package_data=True,
     keywords=['model'],
     classifiers = [
@@ -49,7 +38,7 @@ setup(
         'Topic :: Scientific/Engineering',
         'Programming Language :: Python'
         ],
-    #include_dirs = [np.get_include()],
-    install_requires = ['six','KMlogger','numpy','cython','emcee','corner'],
-    #ext_modules=[]
+    #include_dirs=['.','./ExoSOFTmodel'],
+    install_requires = ['six','KMlogger','numpy','emcee','corner'],
+    ext_modules=[ Extension("cytools",["ExoSOFTmodel/cytools.c"]) ]
 )
